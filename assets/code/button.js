2f5341a62d13bc38d5dd171e5b429e1dc385d1fb
Nizul Zaim <nizulzaim@users.noreply.github.com>
2017-05-31 21:13:41 +0800

    Add Button code

--- assets/code/button.js
+++ assets/code/button.js
@@ -1,4 +1,29 @@
 export const Button1 = `<template>
-    <veb-button v-ripple class="primary" button-style="raised">Testing</veb-button>
+    <veb-button button-style="flat">Button</veb-button>
+    <veb-button button-style="flat" v-ripple>Button</veb-button>
+    <veb-button button-style="flat" class="primary" v-ripple>Button</veb-button>
+    <veb-button button-style="flat" class="accent" v-ripple>Button</veb-button>
+    <veb-button button-style="flat" class="color-green" v-ripple>Button</veb-button>
+    <veb-button button-style="flat" disabled>Button</veb-button>
+</template>
+`
+
+export const Button2 = `<template>
+    <veb-button button-style="raised">Button</veb-button>
+    <veb-button button-style="raised" v-ripple>Button</veb-button>
+    <veb-button button-style="raised" class="primary" v-ripple>Button</veb-button>
+    <veb-button button-style="raised" class="accent" v-ripple>Button</veb-button>
+    <veb-button button-style="flat" class="color-white background-green" v-ripple>Button</veb-button>
+    <veb-button button-style="raised" disabled>Button</veb-button>
+</template>
+`
+
+export const Button3 = `<template>
+    <veb-button>Button</veb-button>
+    <veb-button v-ripple>Button</veb-button>
+    <veb-button class="primary" v-ripple>Button</veb-button>
+    <veb-button class="accent" v-ripple>Button</veb-button>
+    <veb-button button-style="flat" class="color-white background-green" v-ripple>Button</veb-button>
+    <veb-button disabled>Button</veb-button>
 </template>
 `
